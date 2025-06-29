241e3cb6618341db5e5d4100dbf1067461b24668
suttipat-kong <suttipatsurisan@gmail.com>
2025-06-29 16:51:43 +0700

    update schema migration

--- database/migrations/1751189295048_create_logs_table.ts
+++ database/migrations/1751189295048_create_logs_table.ts
@@ -1,12 +1,15 @@
 import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'timesettings'
+  protected tableName = 'logs'
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.time('time_id')
+      table.integer('employee').unsigned().references('id').inTable('users')
+      table.integer('customer').unsigned().references('id').inTable('users')
+      table.integer('time').unsigned().references('id').inTable('times')
+      table.integer('work_status').unsigned().references('id').inTable('work_statuses')
       table.timestamp('created_at')
       table.timestamp('updated_at')
     })
